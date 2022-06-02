1299b5b86efcdf242d5710047ba2f8b1c3eaa1fb
rez13 <cist.b220.y.katou@gmail.com>
2022-06-02 22:56:29 +0900

    サインアップページを追加

--- frontend/App.js
+++ frontend/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from "./src/pages/Home";
-import Login from "./src/pages/login";
+import Login from "./src/pages/Login";
+import Signup from './src/pages/Signup';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
       </Stack.Navigator>
     </NavigationContainer>
   );
