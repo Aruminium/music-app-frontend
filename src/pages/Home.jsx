35e632c9b99863a5f956f9952664c618326f5b2c
[taiga0412] <b2202410@photon.chitose.ac.jp>
2022-06-04 21:36:53 +0900

    ボタンの間隔をつくった

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
-import {Button, NativeBaseProvider,Center} from "native-base";
+import {Button, NativeBaseProvider,Center, HStack} from "native-base";
 
 const Home = ({ navigation }) => {
   return (
     <NativeBaseProvider>
+      
       <View style={styles.container}>
-        <Text>スマートな音楽ライフを</Text>
+        <Text style={{textAlign:'center',fontSize:30}}>スマートミュージックライフ</Text>
         <Button onPress={() =>navigation.navigate("Login")}>ログイン</Button>
+        <View style={styles.space} />
         <Button onPress={() => {navigation.navigate("Signup")}}>サインアップ</Button>
+        <View style={styles.space} />
         <Button onPress={() =>{navigation.navigate("MyPage")}}> マイページ</Button>
+        <View style={styles.space} />
         <Button onPress={() => navigation.navigate("MyCalendar")}>カレンダー</Button>
+        <View style={styles.space} />
         <View style={styles.footer}><Text>©スパゲッティコーダーズ</Text></View>
+       
       </View>
+      
     </NativeBaseProvider>
   );
 };
@@ -33,7 +40,11 @@ const styles = StyleSheet.create({
 
     position: 'absolute',
     bottom: 0 
-  }
+  },
+  space: {
+    width: 20, // or whatever size you need
+    height: 20,
+  },
 });
 
 export default Home;
